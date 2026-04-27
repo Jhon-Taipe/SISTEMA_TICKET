25859368b1a7ffc9f8a6b183995651ee7555f391
Jhon-Taipe <jhonvtaipe@gmail.com>
2026-04-27 11:44:12 -0500

    fix render port

--- app.py
+++ app.py
@@ -78,4 +78,8 @@ def guardar_mensaje(id):
 
     return redirect("/tickets")
 
-app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
